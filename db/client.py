13099c2b49258a1a6a7b818d345f971b4bbcf67d
Diego <diegosof28512@gmail.com>
2026-03-27 17:17:28 -0600

    mongodb atlas

--- db/client.py
+++ db/client.py
@@ -1,4 +1,8 @@
 from pymongo import MongoClient
 
 
-db_client = MongoClient()
+# db_client = MongoClient().local
+
+db_client = MongoClient("mongodb+srv://test:test@cluster0.flhjskh.mongodb.net/?appName=Cluster0").test
+
+# mongodb+srv://diegosof:sofita2812@cluster0.arndsyb.mongodb.net/
